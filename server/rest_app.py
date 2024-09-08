8d822cb960c91d19c8fdb022066bed5bf508be56
alxionescu <alexandru.ioan.ionescu@gmail.com>
2024-09-08 15:57:04 +0300

    post_0
    post ~solved
    clients.csv

--- server/rest_app.py
+++ server/rest_app.py
@@ -1,10 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+
 
 check_app = Flask(__name__)
 
-@check_app.route('/')
-def index():
-    return jsonify({"message": "hello world"})
 
-""" if __name__ == '__main__':
-    check_app.run() """
+@check_app.route('/status', methods=['GET'])
+def status():
+    return jsonify({"OK": "yes"})
+@check_app.route('/sales', methods=['POST'])
+def client():
+        res=  request.form
+        dct= res.to_dict(flat=True)
+        print(dct)
+        with open ("clients.csv", "a") as f:
+            for  key, val in dct.items():
+                f.write (key + ";" + val)
+        f.close()
+              
+        #print (res)
+        return res
+
+check_app.run(debug=1)
+
+
+
